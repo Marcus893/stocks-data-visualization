3d58513a22743067890164b268e019b63d7bf0c8
Caida <9485792@gmail.com>
2019-01-01 22:15:04 -0500

    added more features

--- README.md
+++ README.md
@@ -4,15 +4,14 @@
 ### Background
 This visulization is for the stock market, every stock is visualized by its market cap and price per share
 
-### Functionality & MVP 
+### Functionality & MVP
 with this visualization, users will be able to:
 - [ ] select the sectors and see all the stocks within those sectors on the graph
-- [ ] move cursor to the stock's circle to see detailed info 
+- [ ] move cursor to the stock's circle to see detailed info
 
-### Architechture and Technologies 
+### Architechture and Technologies
 This project is implemented with the following technologies:
-- `JavaScript` for logic,
+- `JavaScript` for the main logic,
 - `HTML`, `CSS` and `d3` for frontend
 
 ![screenshot from 2018-10-17 15-27-04](https://user-images.githubusercontent.com/38970716/47112104-5f569c00-d223-11e8-8304-a2e4f9eeaedb.png)
-
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var d3 = require("d3");
+let d3 = require("d3");
 
 d3.csv('financials.csv', function(data) {
   console.log(data)
-  // Variables
-  var body = d3.select('#chart')
-	var margin = { top: 50, right: 50, bottom: 50, left: 80 }
-	var h = 800 - margin.top - margin.bottom
-	var w = 1000 - margin.left - margin.right
+  // letiables
+  let body = d3.select('#chart')
+	let margin = { top: 50, right: 50, bottom: 50, left: 80 }
+	let h = 600 - margin.top - margin.bottom
+	let w = 600 - margin.left - margin.right
 	// Scales
-  var colorScale = d3.scale.category20();
-  var xScale = d3.scale.linear()
+  let colorScale = d3.scale.category20();
+  let xScale = d3.scale.linear()
     .domain([
     	d3.min(data,function (d) { return parseInt(d.price) }),
     	d3.max(data,function (d) { return parseInt(d.price) })
     	])
     .range([0,w])
     .clamp(true);
-  var yScale = d3.scale.linear()
+  let yScale = d3.scale.linear()
     .domain([
     	d3.min(data,function (d) { return parseInt(d.cap)/1000000 }),
     	d3.max(data,function (d) { return parseInt(d.cap)/1000000 })
@@ -24,18 +24,18 @@ d3.csv('financials.csv', function(data) {
     .range([h,0])
     .clamp(true);
 	// SVG
-	var svg = body.append('svg')
+	let svg = body.append('svg')
 	    .attr('height',h + margin.top + margin.bottom)
 	    .attr('width',w + margin.left + margin.right)
 	  .append('g')
 	    .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
 	// X-axis
-	var xAxis = d3.svg.axis()
+	let xAxis = d3.svg.axis()
 	  .scale(xScale)
 	  .ticks(20)
 	  .orient('bottom')
   // Y-axis
-	var yAxis = d3.svg.axis()
+	let yAxis = d3.svg.axis()
 	  .scale(yScale)
 	  .ticks(10)
 	  .orient('left')
@@ -44,7 +44,7 @@ d3.csv('financials.csv', function(data) {
   d3.selectAll(".secFilter").on("change", update);
   update();
   function update() {
-    var choices = [];
+    let choices = [];
     d3.selectAll(".secFilter").each(function(d) {
       cb = d3.select(this);
       if(cb.property("checked")){
@@ -113,3 +113,286 @@ d3.csv('financials.csv', function(data) {
       .style('text-anchor','end')
       .text('Market Cap / Million')
 })
+
+
+
+
+
+
+(function() {
+  let margin = {top: 30, right: 20, bottom: 100, left: 50},
+    margin2  = {top: 210, right: 20, bottom: 20, left: 50},
+    width    = 764 - margin.left - margin.right,
+    height   = 283 - margin.top - margin.bottom,
+    height2  = 283 - margin2.top - margin2.bottom;
+
+  let parseDate = d3.time.format('%d/%m/%Y').parse,
+    bisectDate = d3.bisector(function(d) { return d.date; }).left,
+    legendFormat = d3.time.format('%b %d, %Y');
+
+  let x = d3.time.scale().range([0, width]),
+    x2  = d3.time.scale().range([0, width]),
+    y   = d3.scale.linear().range([height, 0]),
+    y1  = d3.scale.linear().range([height, 0]),
+    y2  = d3.scale.linear().range([height2, 0]),
+    y3  = d3.scale.linear().range([60, 0]);
+
+  let xAxis = d3.svg.axis().scale(x).orient('bottom'),
+    xAxis2  = d3.svg.axis().scale(x2).orient('bottom'),
+    yAxis   = d3.svg.axis().scale(y).orient('left');
+
+  let priceLine = d3.svg.line()
+    .interpolate('monotone')
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.price); });
+
+  let avgLine = d3.svg.line()
+    .interpolate('monotone')
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.average); });
+
+  let area2 = d3.svg.area()
+    .interpolate('monotone')
+    .x(function(d) { return x2(d.date); })
+    .y0(height2)
+    .y1(function(d) { return y2(d.price); });
+
+  let svg = d3.select('#linechart').append('svg')
+    .attr('class', 'chart')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom + 60);
+
+  svg.append('defs').append('clipPath')
+    .attr('id', 'clip')
+  .append('rect')
+    .attr('width', width)
+    .attr('height', height);
+
+  let make_y_axis = function () {
+    return d3.svg.axis()
+      .scale(y)
+      .orient('left')
+      .ticks(3);
+  };
+
+  let focus = svg.append('g')
+    .attr('class', 'focus')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  let barsGroup = svg.append('g')
+    .attr('class', 'volume')
+    .attr('clip-path', 'url(#clip)')
+    .attr('transform', 'translate(' + margin.left + ',' + (margin.top + 60 + 20) + ')');
+
+  let context = svg.append('g')
+    .attr('class', 'context')
+    .attr('transform', 'translate(' + margin2.left + ',' + (margin2.top + 60) + ')');
+
+  let legend = svg.append('g')
+    .attr('class', 'chart__legend')
+    .attr('width', width)
+    .attr('height', 30)
+    .attr('transform', 'translate(' + margin2.left + ', 10)');
+
+  legend.append('text')
+    .attr('class', 'chart__symbol')
+    .text('NASDAQ: AAPL')
+
+  let rangeSelection =  legend
+    .append('g')
+    .attr('class', 'chart__range-selection')
+    .attr('transform', 'translate(110, 0)');
+
+  d3.csv('./aapl.csv', type, function(err, data) {
+    let brush = d3.svg.brush()
+      .x(x2)
+      .on('brush', brushed);
+
+    let xRange = d3.extent(data.map(function(d) { return d.date; }));
+
+    x.domain(xRange);
+    y.domain(d3.extent(data.map(function(d) { return d.price; })));
+    y3.domain(d3.extent(data.map(function(d) { return d.price; })));
+    x2.domain(x.domain());
+    y2.domain(y.domain());
+
+    let min = d3.min(data.map(function(d) { return d.price; }));
+    let max = d3.max(data.map(function(d) { return d.price; }));
+
+    let range = legend.append('text')
+      .text(legendFormat(new Date(xRange[0])) + ' - ' + legendFormat(new Date(xRange[1])))
+      .style('text-anchor', 'end')
+      .attr('transform', 'translate(' + width + ', 0)');
+
+    focus.append('g')
+        .attr('class', 'y chart__grid')
+        .call(make_y_axis()
+        .tickSize(-width, 0, 0)
+        .tickFormat(''));
+
+    let averageChart = focus.append('path')
+        .datum(data)
+        .attr('class', 'chart__line chart__average--focus line')
+        .attr('d', avgLine);
+
+    let priceChart = focus.append('path')
+        .datum(data)
+        .attr('class', 'chart__line chart__price--focus line')
+        .attr('d', priceLine);
+
+    focus.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0 ,' + height + ')')
+        .call(xAxis);
+
+    focus.append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(12, 0)')
+        .call(yAxis);
+
+    let focusGraph = barsGroup.selectAll('rect')
+        .data(data)
+      .enter().append('rect')
+        .attr('class', 'chart__bars')
+        .attr('x', function(d, i) { return x(d.date); })
+        .attr('y', function(d) { return 155 - y3(d.price); })
+        .attr('width', 1)
+        .attr('height', function(d) { return y3(d.price); });
+
+    let helper = focus.append('g')
+      .attr('class', 'chart__helper')
+      .style('text-anchor', 'end')
+      .attr('transform', 'translate(' + width + ', 0)');
+
+    let helperText = helper.append('text')
+
+    let priceTooltip = focus.append('g')
+      .attr('class', 'chart__tooltip--price')
+      .append('circle')
+      .style('display', 'none')
+      .attr('r', 2.5);
+
+    let averageTooltip = focus.append('g')
+      .attr('class', 'chart__tooltip--average')
+      .append('circle')
+      .style('display', 'none')
+      .attr('r', 2.5);
+
+    let mouseArea = svg.append('g')
+      .attr('class', 'chart__mouse')
+      .append('rect')
+      .attr('class', 'chart__overlay')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+      .on('mouseover', function() {
+        helper.style('display', null);
+        priceTooltip.style('display', null);
+        averageTooltip.style('display', null);
+      })
+      .on('mouseout', function() {
+        helper.style('display', 'none');
+        priceTooltip.style('display', 'none');
+        averageTooltip.style('display', 'none');
+      })
+      .on('mousemove', mousemove);
+
+    context.append('path')
+        .datum(data)
+        .attr('class', 'chart__area area')
+        .attr('d', area2);
+
+    context.append('g')
+        .attr('class', 'x axis chart__axis--context')
+        .attr('y', 0)
+        .attr('transform', 'translate(0,' + (height2 - 22) + ')')
+        .call(xAxis2);
+
+    context.append('g')
+        .attr('class', 'x brush')
+        .call(brush)
+      .selectAll('rect')
+        .attr('y', -6)
+        .attr('height', height2 + 7);
+
+    function mousemove() {
+      let x0 = x.invert(d3.mouse(this)[0]);
+      let i = bisectDate(data, x0, 1);
+      let d0 = data[i - 1];
+      let d1 = data[i];
+      let d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+      helperText.text(legendFormat(new Date(d.date)) + ' - Price: ' + d.price + ' Avg: ' + d.average);
+      priceTooltip.attr('transform', 'translate(' + x(d.date) + ',' + y(d.price) + ')');
+      averageTooltip.attr('transform', 'translate(' + x(d.date) + ',' + y(d.average) + ')');
+    }
+
+    function brushed() {
+      let ext = brush.extent();
+      if (!brush.empty()) {
+        x.domain(brush.empty() ? x2.domain() : brush.extent());
+        y.domain([
+          d3.min(data.map(function(d) { return (d.date >= ext[0] && d.date <= ext[1]) ? d.price : max; })),
+          d3.max(data.map(function(d) { return (d.date >= ext[0] && d.date <= ext[1]) ? d.price : min; }))
+        ]);
+        range.text(legendFormat(new Date(ext[0])) + ' - ' + legendFormat(new Date(ext[1])))
+        focusGraph.attr('x', function(d, i) { return x(d.date); });
+
+        let days = Math.ceil((ext[1] - ext[0]) / (24 * 3600 * 1000))
+        focusGraph.attr('width', (40 > days) ? (40 - days) * 5 / 6 : 5)
+      }
+
+      priceChart.attr('d', priceLine);
+      averageChart.attr('d', avgLine);
+      focus.select('.x.axis').call(xAxis);
+      focus.select('.y.axis').call(yAxis);
+    }
+
+    let dateRange = ['1w', '1m', '3m', '6m', '1y', '5y']
+    for (let i = 0, l = dateRange.length; i < l; i ++) {
+      let v = dateRange[i];
+      rangeSelection
+        .append('text')
+        .attr('class', 'chart__range-selection')
+        .text(v)
+        .attr('transform', 'translate(' + (18 * i) + ', 0)')
+        .on('click', function(d) { focusOnRange(this.textContent); });
+    }
+
+    function focusOnRange(range) {
+      let today = new Date(data[data.length - 1].date)
+      let ext = new Date(data[data.length - 1].date)
+
+      if (range === '1m')
+        ext.setMonth(ext.getMonth() - 1)
+
+      if (range === '1w')
+        ext.setDate(ext.getDate() - 7)
+
+      if (range === '3m')
+        ext.setMonth(ext.getMonth() - 3)
+
+      if (range === '6m')
+        ext.setMonth(ext.getMonth() - 6)
+
+      if (range === '1y')
+        ext.setFullYear(ext.getFullYear() - 1)
+
+      if (range === '5y')
+        ext.setFullYear(ext.getFullYear() - 5)
+
+      brush.extent([ext, today])
+      brushed()
+      context.select('g.x.brush').call(brush.extent([ext, today]))
+    }
+
+  })// end Data
+
+  function type(d) {
+    return {
+      date    : parseDate(d.Date),
+      price   : +d.Close,
+      average : +d.Average,
+      volume : +d.Volume,
+    }
+  }
+}());
